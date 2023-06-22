7af630318448f8c80d60b71aed36360620d41996
Troi Williams <40696868+troiwill@users.noreply.github.com>
2023-06-22 17:14:10 -0400

    Fixed EKF uncertainty and added READY/ACK signals (#3)
    * Updated the rendezvous machine so that it exits immediately after execution.
    
    * Reimplemented EKF to be similar to robot_localization EKF.
    
    * Updated Rviz
    
    * Updated launch files with covariances.
    
    * Added READY and ACK signals for TB3.

--- src/senseless_robot/states/listen_for_meas.py
+++ src/senseless_robot/states/listen_for_meas.py
@@ -2,11 +2,14 @@ from threading import Lock
 from geometry_msgs.msg import PoseWithCovarianceStamped
 import rospy
 import smach
+from std_msgs.msg import String
 
 
 class ListenForMeasurement(smach.State):
     OC_RECEIVED = "received"
     OC_FAILURE = "failure"
+
+    OK_POSE_MEASUREMENTS = "pose_measurements"
 
     def __init__(self):
         super().__init__(
@@ -14,7 +17,7 @@ class ListenForMeasurement(smach.State):
                 ListenForMeasurement.OC_RECEIVED,
                 ListenForMeasurement.OC_FAILURE,
             ],
-            output_keys=["pose_measurements"],
+            output_keys=[ListenForMeasurement.OK_POSE_MEASUREMENTS],
         )
 
         # Gather the ROS parameters.
@@ -23,6 +26,7 @@ class ListenForMeasurement(smach.State):
         wait_timeout = float(rospy.get_param("listen_wait_timeout", 30.0))
         max_messages = int(rospy.get_param("listen_max_messages", 1))
         listen_delay = float(rospy.get_param("listen_delay", 0.0))
+        meas_comms_topic = rospy.get_param("meas_comms_topic")
 
         # Sanity checks.
         rospy.logdebug("Performing the sanity checks.")
@@ -30,6 +34,7 @@ class ListenForMeasurement(smach.State):
         assert isinstance(wait_timeout, (int, float))
         assert isinstance(max_messages, int)
         assert isinstance(listen_delay, float)
+        assert isinstance(meas_comms_topic, str)
         assert wait_timeout > 0.0
         assert max_messages > 0
         assert listen_delay >= 0.0
@@ -49,6 +54,8 @@ class ListenForMeasurement(smach.State):
         self.wait_timeout = wait_timeout
         self.max_messages = max_messages
         self.listen_delay = listen_delay
+
+        self._is_ready_pub = rospy.Publisher(meas_comms_topic, String, queue_size=1)
 
     def execute(self, ud):
         outcome: str = ListenForMeasurement.OC_RECEIVED
@@ -72,12 +79,14 @@ class ListenForMeasurement(smach.State):
                 self.is_listening_for_measurements = True
 
             # Wait for measurements.
+            rospy.logdebug("Publishing the READY message, and waiting for measurements.")
             while True:
                 time_elapsed = rospy.get_time() - self.listen_start_time
                 if (
                     self.is_listening_for_measurements == True
                     and time_elapsed < self.wait_timeout
                 ):
+                    self._is_ready_pub.publish(String("READY"))
                     rospy.sleep(0.1)
                 else:
                     break

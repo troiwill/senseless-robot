43007f642e7bb28c041017805938910f76122e09
Troi Williams <40696868+troiwill@users.noreply.github.com>
2023-06-22 17:04:35 -0400

    Added READY and ACK signals for TB3.

--- src/senseless_robot/states/__init__.py
+++ src/senseless_robot/states/__init__.py
@@ -1,3 +1,4 @@
+from .ack_meas import AcknowledgeMeasurement
 from .compute_next_pose import ComputeNextPose
 from .listen_for_meas import ListenForMeasurement
 from .move_to_rendezvous_pose import MoveToRendezvousPose
---
+++ src/senseless_robot/states/ack_meas.py
@@ -0,0 +1,43 @@
+from std_msgs.msg import String
+import rospy
+import smach
+
+
+class AcknowledgeMeasurement(smach.State):
+    OC_SENT = "sent"
+    OC_FAILURE = "failure"
+
+    def __init__(self):
+        super().__init__(
+            outcomes=[
+                AcknowledgeMeasurement.OC_SENT,
+                AcknowledgeMeasurement.OC_FAILURE,
+            ],
+        )
+
+        # Gather the ROS parameters.
+        rospy.logdebug("Gathering the ROS parameters.")
+        meas_comms_topic = rospy.get_param("meas_comms_topic")
+
+        # Sanity checks.
+        rospy.logdebug("Performing the sanity checks.")
+        assert isinstance(meas_comms_topic, str)
+        
+        # Create a publisher for acknowledging measurements.
+        self._ack_meas_pub = rospy.Publisher(meas_comms_topic, String, queue_size=1)
+
+    def execute(self, ud):
+        outcome: str = AcknowledgeMeasurement.OC_SENT
+
+        try:
+            # Publish ACK messages.
+            rospy.logdebug("Publishing the ACK message!")
+            for _ in range(10):
+                self._ack_meas_pub.publish(String("ACK"))
+                rospy.sleep(0.01)
+
+        except:
+            outcome = AcknowledgeMeasurement.OC_FAILURE
+
+        rospy.loginfo(f"{AcknowledgeMeasurement.__name__} outcome: {outcome}")
+        return outcome
